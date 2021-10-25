4a63371f310b232615750390fbdb6ece57111ced
vishalmeti <74894524+vishalmeti@users.noreply.github.com>
2021-10-25 20:55:23 +0530

    Update settings.py

--- studybuddy/settings.py
+++ studybuddy/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_qlna!qn4i^7kuyxtf1n12$2wqlt28u6s@k5jt=$nq=m0l@!i5'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
